cdab21106f14af3c0a058e2f74f3537d28261f6e
Borek2002 <damianogdf@gmail.com>
2024-11-09 20:37:19 +0100

    added entity, endpoint and improve genarator

--- SmogGenerator/main.py
+++ SmogGenerator/main.py
@@ -6,11 +6,11 @@ from datetime import datetime, timedelta
 from enum import Enum
 import pika
 
-
 USERNAME = 'guest'
 PASSWORD = 'guest'
 PORT = 5672
 
+startDate = datetime(2024, 10, 20)
 
 class Type(Enum):
     TYPE1 = 0
@@ -18,13 +18,12 @@ class Type(Enum):
     TYPE3 = 2
     TYPE4 = 3
 
-
 class Station:
-    def __init__(self, stationUUID, st_type, value):
+    def __init__(self, stationUUID, st_type, value, last_message):
         self.stationUUID = stationUUID
         self.type = st_type
         self.value = value
-
+        self.lastMessage = last_message
 
 class Message:
     def __init__(self, messageUUID, station, dateTime):
@@ -41,7 +40,6 @@ class Message:
             'value': self.station.value
         }
 
-
 station_uuids = ['cf7ede9d-eb8e-426f-a101-999d7425c26e', '4ca91a6d-9c17-4b0c-82c0-4399c2f65adb',
                  '3b2ee09e-f753-4124-b403-f862c3628f88', 'bf8ab9d0-8894-4d0a-9616-7e989add1ef9',
                  '1b8227fc-7729-4319-b094-1c4afda6d020', '6a5497ae-c8c4-45b0-b233-d9ef1e979837',
@@ -49,10 +47,10 @@ station_uuids = ['cf7ede9d-eb8e-426f-a101-999d7425c26e', '4ca91a6d-9c17-4b0c-82c
                  'a139eb4f-8017-4c42-8556-f5f1c33ab041', '710a49a9-39c6-45ab-bc5b-e6cbfa6a240b',
                  'f959ce51-5e57-4a22-a711-8620324704e4', '8ee1c2a9-cad9-4ffc-af8a-41c66031092e',
                  '854d1ecd-6fb8-4120-bcb4-e0d34ba2eede', 'c68ee8f2-6345-465c-a00c-2e99c148d7f4',
-                 'cdb05bb6-13a6-48c8-9494-0c675eae79d4', 'ab2bf1e4-3c5d-42df-bd6e-04d5ceba2fcb']
+                 'cdb05bb6-13a6-48c8-9494-0c675eae79d4', 'ab2bf1e4-3c5d-42df-bd6e-04d5ceba2eecb']
+
 stations = []
 connection = None
-
 
 def initConnection():
     global connection
@@ -68,27 +66,63 @@ def sendMessage(mess):
     channel.basic_publish(exchange='', routing_key='sensorQueue', body=json.dumps(mess.to_JSON()))
     channel.close()
 
-
 def initStations():
     for i in range(len(station_uuids)):
         uuid = station_uuids[i]
         type = Type(i % len(Type))
-        station = Station(uuid, type, None)
+        station = Station(uuid, type, None, startDate)
         stations.append(station)
 
+def prepareMessage(station, dateTime):
+    messageUUID = uuid.uuid4()
+    formatted_datetime = dateTime.strftime('%Y-%m-%dT%H:%M:%S')
+    message = Message(messageUUID, station, formatted_datetime)
+    print(message.to_JSON())
+    return message
+
+def setLastMessage(station):
+    if station.type == Type.TYPE1:
+        station.lastMessage = station.lastMessage + timedelta(hours=1)
+    elif station.type == Type.TYPE2:
+        station.lastMessage = station.lastMessage + timedelta(hours=2)
+    elif station.type == Type.TYPE3:
+        station.lastMessage = station.lastMessage + timedelta(hours=3)
+    elif station.type == Type.TYPE4:
+        station.lastMessage = station.lastMessage + timedelta(hours=4)
 
 initStations()
 initConnection()
 
-startDate = datetime(2024, 10,20)
-while True:
-    sleep_time = random.uniform(1, 5)  # Losowa liczba zmiennoprzecinkowa od 1 do 5
-    print(f"Sleeping for {sleep_time:.2f} seconds...")
-    time.sleep(sleep_time)
-    station = stations[random.randint(0, len(stations) - 1)]
-    station.value = random.randint(0, 30)
-    messageUUID = uuid.uuid4()
-    startDate += timedelta(hours=4)
-    message = Message(messageUUID, station, startDate)
-    print(message.to_JSON())
+lastDate =[startDate, startDate, startDate, startDate]
 
+print("Witaj w generatorze danych :)")
+print("Generuj dane dla wszystkich stacji - a")
+print("Generuj dane dla stacji numer uuid - s")
+print("Zakończ program - q")
+user_input = input("Wybierz opcję (a/s): ")
+station_found = None
+
+while True:
+    if user_input == 'a':
+        for station in stations:
+            station.value = random.randint(0, 30)
+            setLastMessage(station)
+            sendMessage(prepareMessage(station, station.lastMessage))
+    elif user_input == 's':
+        if(station_found == None):
+            uuid_input = input("Podaj UUID stacji: ")
+            for station in stations:
+                if station.stationUUID == uuid_input:
+                    station_found = station
+                    break
+        if station_found:
+            station_found.value = random.randint(0, 30)
+            setLastMessage(station_found)
+            sendMessage(prepareMessage(station_found, station_found.lastMessage))
+        else:
+            print("Nie znaleziono stacji o podanym UUID.")
+    elif user_input == 'q':
+        print("Zatrzymywanie generatora...")
+        break
+    else:
+        print("Niepoprawny wybór, spróbuj ponownie.")
